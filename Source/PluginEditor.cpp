effd91ef0dac54b8c0286ea5c9d2d36be9e768b3
Denise N <denise.ndalamba@gmail.com>
2025-07-26 19:52:00 -0600

    add playing button

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -10,8 +10,22 @@ AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor (AudioPluginAud
     // editor's size to whatever you need it to be.
 
     frequencySlider.setSliderStyle (juce::Slider::SliderStyle::LinearVertical);
-    frequencySlider.setTextBoxStyle(juce::Slider::TextBoxBelow, true, 100, 50);
+    frequencySlider.setTextBoxStyle(juce::Slider::TextBoxBelow, true, 50, 25);
+    frequencySlider.setRange(0.0f, 1.0f, 0.01f);
     addAndMakeVisible(frequencySlider);
+
+    // play button
+    playButton.setButtonText("Playing");
+    playButton.setToggleState(true, juce::NotificationType::dontSendNotification);
+    playButton.setClickingTogglesState(true);
+
+    playButton.onClick = [this]()
+    {
+        // change state of button when clicked
+        const bool isPlaying = playButton.getToggleState();
+        playButton.setButtonText(isPlaying ? "Playing" : "Bypassed");
+    };
+    addAndMakeVisible(playButton);
 
     setSize (400, 400);
 }
@@ -36,4 +50,5 @@ void AudioPluginAudioProcessorEditor::resized()
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
     frequencySlider.setBounds(getWidth() / 2 - 50, getHeight() / 2 - 100, 100, 200);
+    playButton.setBounds(getWidth() / 2 - 50, getHeight() / 2 + 120, 100, 20);
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -17,6 +17,7 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     juce::Slider frequencySlider;
+    juce::TextButton playButton;
 
     AudioPluginAudioProcessor& processorRef;
 
